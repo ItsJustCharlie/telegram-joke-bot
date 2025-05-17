bc884a755f0446942e4a121d12f2a2aa0bd76c1a
Samindi Liyanage <umesha.liyanage.lk@gmail.com>
2025-05-17 23:33:49 +0530

    commit changes

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["LANGCHAIN_PROJECT"] = "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"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-groq_api_key = os.getenv("GROQ_API_KEY")
+groq_api_key = "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"
 
 def setup_llm_chain(topic="technology"):
    
@@ -29,30 +29,39 @@ def setup_llm_chain(topic="technology"):
     return prompt|llm|StrOutputParser()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hi! Mention me with a topic like '@Binary_Joke_Bot python' to get a joke ")
-    
+    if update.message:
+        await update.message.reply_text("Hi! Mention me with a topic like '@TellMeJokeBot python' to get a joke")
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Mention me with a topic like '@Binary_Joke_Bot python', to get some funny jokes")
-    
+    if update.message:
+        await update.message.reply_text("Mention me with a topic like '@TellMeJokeBot python', to get some funny jokes")
+
 async def generate_joke(update: Update, context: ContextTypes.DEFAULT_TYPE, topic: str):
-    await update.message.reply_text(f"Generating a joke about {topic}")
-    joke= setup_llm_chain(topic).invoke({}).strip()
-    await update.message.reply_text(joke)
-    
+    if update.message:
+        await update.message.reply_text(f"Generating a joke about {topic}")
+        joke = setup_llm_chain(topic).invoke({}).strip()
+        await update.message.reply_text(joke)
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message:
+        return
+
     msg = update.message.text
     bot_username = context.bot.username
-    
-    if f'@{bot_username}' in msg:
-        match = re.search(f'@{bot_username}\\s+(.*)',msg)
+
+    msg = update.message.text
+    bot_username = context.bot.username
+
+    if msg is not None and f'@{bot_username}' in msg:
+        match = re.search(f'@{bot_username}\\s+(.*)', msg)
         if match and match.group(1).strip():
             await generate_joke(update, context, match.group(1).strip())
         else:
             await update.message.reply_text("Please specify a topic after mentioning me")
-            
+
             
 def main():
-    token = os.getenv("TELEGRAM_API_KEY")
+    token = "7919607597:AAFzbLJxKt98ngISnJzlNSxKspRPnKRQAGU"
     app = Application.builder().token(token).build()
     app.add_handler(CommandHandler("start",start))
     app.add_handler(CommandHandler("help", help_command))
